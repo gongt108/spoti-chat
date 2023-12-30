8113e8188a55026dec37f3806f6923e74e55df9f
keya-moradi <keya.moradi@gmail.com>
2023-12-30 15:32:04 -0800

    test

---
+++ pages/recommendation.js
@@ -0,0 +1,15 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// // import useAuth from './useAuth';
+// import styles from '../styles/Recommendation.module.css';
+
+// import SpotifyWebApi from 'spotify-web-api-node';
+// import { useSearchParams } from 'next/navigation';
+// import RecommendationNav from '../components/recomendationNav';
+// import SearchResults from '../components/recommendationResults';
+// import cookie from 'js-cookie';
+
+// const spotifyApi = new SpotifyWebApi({
+// 	clientId: process.env.SPOTIFY_CLIENT_ID,
+// });
+
