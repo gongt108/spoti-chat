cd1245d9388d211af1855655ccd83f5f44d493da
keya-moradi <keya.moradi@gmail.com>
2024-01-03 21:43:28 -0800

    add comments for the recommendations.js file

--- pages/recommendations.js
+++ pages/recommendations.js
@@ -1,36 +1,41 @@
+// Importing the necessary modules and components
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import useAuth from './useAuth';
 import styles from '../styles/Recommendation.module.css';
-
 import SpotifyWebApi from 'spotify-web-api-node';
 import RecommendationNav from '../components/recommendationNav';
 import RecommendationResults from '../components/recommendationResults';
 import cookie from 'js-cookie';
 import { useSearchParams } from 'next/navigation';
 
+// Creating a Spotify Web API instance
 const spotifyApi = new SpotifyWebApi({
 	clientId: process.env.SPOTIFY_CLIENT_ID,
 });
 
+// Defining the Recommendation functional component
 function Recommendation() {
-	// let data = {};
+
+	// Setting up state variables for search query, access token, user ID, and recommendation data
 	const [loading, setLoading] = useState(true);
 	const [data, setData] = useState([]);
-	// const code = recommendationParams.get('code');
-	// get user inputted recommendation term
+
+	// Fetching recommendation parameters from URL search parameters
 	const searchParams = useSearchParams();
 	const recommendationId = searchParams.get('recommendationId') || '';
 	const recommendationType = searchParams.get('recommendationType') || '';
-
-	// retrieve access code from cookies
 	const accessToken = cookie.get('accessToken');
 
-	// get data from Spotify API
+	// useEffect hook to fetch recommendation data from Spotify API
+	//fetching recommendation data from the Spotify API when the component mounts or when the dependencies (recommendationId, recommendationType, accessToken) change.
 	useEffect(() => {
+
+		// Async function to fetch data
 		const fetchData = async () => {
 			setLoading(true);
 			try {
+
+				// Making an HTTP GET request to Spotify API recommendations endpoint
 				const { data: res } = await axios.get(
 					`https://api.spotify.com/v1/recommendations?seed_${recommendationType}s=${recommendationId}`,
 					{
@@ -40,7 +45,11 @@ function Recommendation() {
 						},
 					}
 				);
+
+				// Logging tracks data to the console
 				console.log(res.tracks);
+
+				// Switch statement to set data based on recommendation type
 				switch (recommendationType) {
 					case 'genre':
 						setData(res.tracks);
@@ -55,29 +64,43 @@ function Recommendation() {
 						setData([]);
 				}
 			} catch (error) {
+				// Handling errors, e.g., logging to the console
 				console.error(error);
 			}
+
+			// Updating loading state after data fetch
 			setLoading(false);
 		};
-		fetchData();
-	}, [recommendationId, recommendationType]);
 
+		// Calling the fetchData function
+		fetchData();
+	}, [recommendationId, recommendationType, accessToken]); // The useEffect will re-run whenever these dependencies change
+
+	// JSX representing the Recommendation component
 	return (
 		<div className={styles.recommendationContainer}>
+			{/* Rendering RecommendationNav component */}
 			<RecommendationNav />
+
+			{/* Conditional rendering based on data length */}
 			{data.length <= 0 && (
 				<h3 className={styles.placeholderRecommendationResults}>
-					Type in recommendation term to find results
+					Type in a recommendation term to find results
 				</h3>
 			)}
+
+			{/* Conditional rendering based on loading state and data length */}
 			{!loading && data.length > 0 && (
 				<div className={styles.recommendationContainerResultContainer}>
+					{/* Rendering RecommendationResults component */}
 					<RecommendationResults data={data} recommendationType={recommendationType} />
 				</div>
 			)}
-			{/* <RecommendationResults data={data} /> */}
+			{/* Empty placeholder to be filled if needed */}
+			{/* {} */}
 		</div>
 	);
 }
 
+// Exporting the Recommendation component as the default export
 export default Recommendation;
