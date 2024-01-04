ef86d88c31b759e7740875b283d1e86a8062b4cc
keya-moradi <keya.moradi@gmail.com>
2024-01-03 21:30:29 -0800

    add comments for the recommendationResults.js file

--- components/recommendationResults.js
+++ components/recommendationResults.js
@@ -1,18 +1,25 @@
+
+// Importing necessary modules and components
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from '../styles/Recommendation.module.css';
 import RecommendationNav from './recommendationNav';
-
 import ArtistShareCard from './artistShareCard';
 import AlbumShareCard from './albumShareCard';
 import SongShareCard from './songShareCard';
 
+// Defining the RecommendationResults functional component
 function RecommendationResults({ data, recommendationType }) {
+
+	// Declaring variables to manage the 'Loading' state and 'displayeResults'
 	const [loading, setLoading] = useState(true);
 	const [displayResult, setDisplayResult] = useState();
-
-	// choose display card based on data type
+	
+	// useEffect hook to process and display recommendations when data changes
+	// updating the component state 'displayResult' and 'Loading' based on changes to the data
 	useEffect(() => {
+
+		// Mapping the data to individual SongShareCard components
 		const dataMap = data.map((track, i) => {
 			return (
 				<SongShareCard
@@ -28,20 +35,19 @@ function RecommendationResults({ data, recommendationType }) {
 				/>
 			);
 		});
+
+		// Updating variables to display the mapped results and setting the loading to be false
 		setDisplayResult(dataMap);
 		setLoading(false);
 	}, []);
-
-	// const albumDisplay = data.map((item, idx) => {
-	// 	return <ArtistShareCard />;
-	// });
-
+	
+	// JSX representing the RecommendationResults component
 	return (
 		<div className={styles.recommendationResultContainer}>
-			{/* <Image src="/images/madeon-good-faith.jpg" width={50} height={50} /> */}
 			{!loading && displayResult}
 		</div>
 	);
 }
 
+// Exporting the RecommendationResults component as the default export
 export default RecommendationResults;
