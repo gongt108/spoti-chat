15eb421f60bbd4cfe49bec557ceb8ec8d7022e3a
Tiffany Gong <gongt108@gmail.com>
2024-01-06 11:59:45 -0800

    login and sign up page

--- pages/users/login.js
+++ pages/users/login.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import Image from 'next/image';
 import styles from '../../styles/Login.module.css';
 import axios from 'axios';
 import { useRouter } from 'next/router';
@@ -30,7 +30,7 @@ const Login = () => {
 		e.preventDefault();
 		console.log(email);
 		await axios.get(`http://localhost:8000/users/${email}`).then((response) => {
-			console.log(response.data);
+			// console.log(response.data);
 			console.log(password);
 			if (password === response.data.password) {
 				cookie.set('userId', response.data._id, { expires: oneHour });
@@ -49,31 +49,46 @@ const Login = () => {
 
 	return (
 		<div className={styles.mainContainer}>
+			<Image
+				className={styles.image}
+				width={1025}
+				height={940}
+				src={'/images/login-Image.jpeg'}
+				alt="Description of the image"
+			/>
 			<div className={styles.container}>
-				<h1 className={styles.title}>SPOTI-CHAT</h1>
-				<h2 className={styles.header}>Login</h2>
 				<form className={styles.form} onSubmit={handleLogin}>
 					<div className={styles.formGroup}>
-						<label className={styles.label}>Email</label>
+						<label className={styles.label}></label>
 						<input
 							className={styles.input}
 							type="text"
-							value={email}
+							placeholder="Email"
+							name="email"
+							// value={email}
 							onChange={handleEmailChange}
 							required
 						/>
 					</div>
 					<div className={styles.formGroup}>
-						<label className={styles.label}>Password</label>
+						<label className={styles.label}></label>
 						<input
 							className={styles.input}
 							type="password"
-							value={password}
+							placeholder="Password"
+							name="password"
+							// value={password}
 							onChange={handlePasswordChange}
 							required
 						/>
 					</div>
-					<button type="submit">Login</button>
+					<button className={styles.button} type="submit">
+						Login
+					</button>
+					<button className={styles.button} type="submit">
+						Signup
+					</button>
+					<p>Don't have an accout? Sign up!</p>
 				</form>
 				{error && <p className={styles.error}>{error}</p>}
 			</div>
