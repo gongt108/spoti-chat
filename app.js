5dbd56deb82d59906ba305a6725661291b309c1a
Tiffany Gong <gongt108@gmail.com>
2024-01-09 08:08:27 -0800

    edit useAuth uri

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-const faker = require('@faker-js/faker');
-const express = require('express');
-const methodOverride = require('method-override');
-const mongoose = require('mongoose');
-const cors = require('cors');
+// const faker = require('@faker-js/faker');
+// const express = require('express');
+// const methodOverride = require('method-override');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
 
-// create app
-const app = express();
+// // create app
+// const app = express();
 
-// middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
+// // middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(cors());
 
-app.get('/', (req, res) => {
-	return res.json({ message: 'Welcome to my Facebook App' });
-});
+// app.get('/', (req, res) => {
+// 	return res.json({ message: 'Welcome to my Facebook App' });
+// });
 
-// app.use('/games', require('./controllers/games'));
-// app.use('/videos', require('./controllers/videos'));
-// app.use('/users', require('./controllers/users'));
+// const PORT = process.env.PORT || 8000;
+// app.listen(PORT, () => {
+// 	console.log(`Server connected to PORT: ${PORT}`);
+// });
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-	console.log(`Server connected to PORT: ${PORT}`);
-});
-
-module.exports = app;
+// module.exports = app;
