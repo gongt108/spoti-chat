755ffc50e4900c4581761c53c05bef64392802d7
Tiffany Gong <gongt108@gmail.com>
2024-01-09 08:50:31 -0800

    Add callback.js page

---
+++ pages/callback.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/router';
+import styles from '../styles/NoAccess.module.css';
+
+function Callback() {
+	const searchParams = useSearchParams();
+	const router = useRouter();
+	const code = searchParams.get('code');
+
+	return (
+		<div className={styles.container}>
+			<h2>
+				You have successfully logged in. If you are not redirected to the home
+				page within 5 seconds, please press{' '}
+				<Link href={{ pathname: `/`, query: { code: `${code}` } }}>here</Link>
+			</h2>
+		</div>
+	);
+}
+
+export default Callback;
